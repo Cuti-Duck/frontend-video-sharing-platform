e2e4c50ecafdf26ca1420a1d61f1fbd0dde602e7
Cuti-Duck <khanhdnse180587@fpt.edu.vn>
2025-11-29 15:12:05 +0700

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
-import { MainVideoFrame } from "@/components/MainVideoFrame";
+import { VideoCard } from "@/components/VideoCard";
+import VideoApi from "@/lib/videoApi";
+import { VideoItems } from "@/types/video";
 
-export default function Home() {
+export default async function Home() {
+  console.log("Home page")
+  let videos = [];
+  const response = await VideoApi.GetVideosTrending()
+  videos = response.data
 
   return (
     <div>
-      <MainVideoFrame/>
+      {/* <MainVideoFrame/> */}
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+      {videos.map((video: VideoItems) => (
+          <VideoCard key={video.videoId} videoId={video.videoId} layout="vertical" limit={false}/>
+      ))}
+      </div>   
     </div>
   );
 }
